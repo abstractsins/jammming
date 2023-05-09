b0f5b958cf9a24dd3a6742152d30e6cb449be689
Daniel Berlin <daniel.berlin@comcast.net>
2023-05-08 21:04:08 -0400

    Track fix

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,11 +3,10 @@ import React from 'react';
 function Track(props){
 
     return (
-        <div key={props.key}>
+        <li className='track' key={props.key}>
             <h3>{props.title}</h3>
-            <h4>{props.artist}</h4>
-            <h4>{props.album}</h4>
-        </div>
+            <h4>{props.artist} <span className='album'>({props.album})</span></h4>
+        </li>
     )
 
 }
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -9,17 +9,17 @@ function Tracklist(props){
 
     
     let trackListing = Object.entries(tracks).map(track=>
-        <li key={track[1].key} className="track"> 
-            <h3 className="title">"{track[1].name}"</h3>
-            <h4>{track[1].artist} <span className="album">({track[1].album})</span></h4>
-        </li>);
+        <Track 
+            title={track[1].name} 
+            artist={track[1].artist} 
+            album={track[1].album}
+        />
+    );
     
     return (
-        <div>
-            <ul>
-                {trackListing}
-            </ul>
-        </div>
+        <ul>
+            {trackListing}
+        </ul>
     )
 
 }
